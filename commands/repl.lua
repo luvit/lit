2fd1a2fc1a09fcca6adb7813dcc310115789391d
Tim Caswell <tim@creationix.com>
2014-12-13 11:40:40 -0600

    Move to help as default lit command

--- commands/repl.lua
+++
@@ -1,9 +0,0 @@
-local prompt = require('prompt')
-
-coroutine.wrap(function ()
-  -- prompt returns false on Control+C
-  -- and nil on Control+D, assert will catch those and exit the process.
-  local name = assert(prompt("Who are you? "))
-  local age = tonumber(assert(prompt("How old are you? ")))
-  p{name=name,age=age}
-end)()
