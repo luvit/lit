33c39527bbd4fb620f0d67e82658e08fffd6cbb9
Tim Caswell <tim@creationix.com>
2014-12-13 22:55:02 -0600

    Start to implement install

--- commands/install.lua
+++ commands/install.lua
@@ -1,1 +1,31 @@
-error("TODO: Implement install command")
+local log = require('lit-log')
+local config = require('lit-config')
+local uv = require('uv')
+local pathJoin = require('luvi').path.join
+local prompt = require('prompt')
+local export = require('export')
+local storage = require('lit-storage')
+
+
+local name = args[2] or prompt("package name")
+
+if not string.find(name, "/") then
+  name = config["github name"] .. '/' .. name
+end
+
+local version = args[3] or "*"
+
+local target = pathJoin(uv.cwd(), "modules", name)
+
+log("package", name)
+log("version", version)
+log("target", target)
+
+
+storage:names(function (name)
+end)
+-- TODO: match against versions in local database
+
+-- TODO: check upstream if no match can be found locally
+
+-- TODO: export once match is found
