61435467e33a3c9a3e7ac41fe1ff2cb2cd89edb6
Tim Caswell <tim@creationix.com>
2014-12-13 22:43:58 -0600

    Add useful list of commands

--- commands/help.lua
+++ commands/help.lua
@@ -1,1 +1,15 @@
-error("TODO: Implement help command")
+print [[
+Lit - Package Manager for Luvit
+
+Available commands:
+
+  lit install [username/package] [version]
+  lit sync [upstream]
+  lit auth [username] [name] [email]
+  lit add [path] [package] [version] [release message]
+  lit publish [package] [upstream]
+  lit serve
+]]
+
+-- TODO: print detailed help for each command with `help install` or
+-- `help sync` for example.
