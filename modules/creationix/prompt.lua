1183413f7a5cf50ab9d539fd74665a237443d5af
Tim Caswell <tim@creationix.com>
2015-01-05 15:38:17 -0600

    Update prompt to use readline from lit repo

--- modules/creationix/prompt.lua
+++ modules/creationix/prompt.lua
@@ -1,29 +1,34 @@
 exports.name = "creationix/prompt"
-exports.version = "0.1.0"
+exports.version = "0.2.0"
+exports.dependencies = {
+  "creationix/readline@1.0.1"
+}
 
-local readLine = require('readline').readLine
+local readLine = require('creationix/readline').readLine
 
--- Wrapper around readline to provide a nice blocking version for coroutines
-return function (message, default)
-  local thread = coroutine.running()
+return function (options)
+  -- Wrapper around readline to provide a nice blocking version for coroutines
+  return function (message, default)
+    local thread = coroutine.running()
 
-  message = message .. ": "
-  if default then
-    message = message .. "(" .. default .. ") "
-  end
-
-  local value
-  repeat
-    readLine(message, function (err, line, reason)
-      if err then
-        return assert(coroutine.resume(thread, nil, err))
-      end
-      return assert(coroutine.resume(thread, line, reason))
-    end)
-    value = assert(coroutine.yield())
-    if default and #value == 0 then
-      value = default
+    message = message .. ": "
+    if default then
+      message = message .. "(" .. default .. ") "
     end
-  until #value > 0
-  return value
+
+    local value
+    repeat
+      readLine(message, options, function (err, line, reason)
+        if err then
+          return assert(coroutine.resume(thread, nil, err))
+        end
+        return assert(coroutine.resume(thread, line, reason))
+      end)
+      value = assert(coroutine.yield())
+      if default and #value == 0 then
+        value = default
+      end
+    until #value > 0
+    return value
+  end
 end
