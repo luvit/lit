e9282d2f84a3d83e014d6dbbdf157c7bcddaac18
Tim Caswell <tim@creationix.com>
2015-01-08 21:06:52 -0600

    websocket-codec v0.1.2: Cache common functions in upvalues

--- app/modules/creationix/websocket-codec.lua
+++ app/modules/creationix/websocket-codec.lua
@@ -1,71 +1,84 @@
 exports.name = "creationix/websocket-codec"
-exports.version = "0.1.1"
+exports.version = "0.1.2"
 
 local digest = require('openssl').digest.digest
 local base64 = require('openssl').base64
 local random = require('openssl').random
 local hexToBin = require('creationix/hex-bin').hexToBin
 
+local band = bit.band
+local bor = bit.bor
+local bxor = bit.bxor
+local rshift = bit.rshift
+local lshift = bit.lshift
+local char = string.char
+local byte = string.byte
+local sub = string.sub
+local gmatch = string.gmatch
+local lower = string.lower
+local gsub = string.gsub
+local concat = table.concat
+
 local function applyMask(data, mask)
   local bytes = {
-    [0] = string.byte(mask, 1),
-    [1] = string.byte(mask, 2),
-    [2] = string.byte(mask, 3),
-    [3] = string.byte(mask, 4)
+    [0] = byte(mask, 1),
+    [1] = byte(mask, 2),
+    [2] = byte(mask, 3),
+    [3] = byte(mask, 4)
   }
   local out = {}
   for i = 1, #data do
-    out[i] = string.char(
-      bit.bxor(string.byte(data, i), bytes[(i - 1) % 4])
+    out[i] = char(
+      bxor(byte(data, i), bytes[(i - 1) % 4])
     )
   end
-  return table.concat(out)
+  return concat(out)
 end
 
 function exports.decode(chunk)
   if #chunk < 2 then return end
-  local second = string.byte(chunk, 2)
-  local len = bit.band(second, 0x7f)
+  local second = byte(chunk, 2)
+  local len = band(second, 0x7f)
   local offset
   if len == 126 then
     if #chunk < 4 then return end
-    len = bit.bor(
-      bit.lshift(string.byte(chunk, 3), 8),
-      string.byte(chunk, 4))
+    len = bor(
+      lshift(byte(chunk, 3), 8),
+      byte(chunk, 4))
     offset = 4
   elseif len == 127 then
     if #chunk < 10 then return end
-    len = bit.bor(
-      bit.lshift(string.byte(chunk, 3), 56),
-      bit.lshift(string.byte(chunk, 4), 48),
-      bit.lshift(string.byte(chunk, 5), 40),
-      bit.lshift(string.byte(chunk, 6), 32),
-      bit.lshift(string.byte(chunk, 7), 24),
-      bit.lshift(string.byte(chunk, 8), 16),
-      bit.lshift(string.byte(chunk, 9), 8),
-      string.byte(chunk, 10))
+    len = bor(
+      lshift(byte(chunk, 3), 56),
+      lshift(byte(chunk, 4), 48),
+      lshift(byte(chunk, 5), 40),
+      lshift(byte(chunk, 6), 32),
+      lshift(byte(chunk, 7), 24),
+      lshift(byte(chunk, 8), 16),
+      lshift(byte(chunk, 9), 8),
+      byte(chunk, 10))
     offset = 10
   else
     offset = 2
   end
-  local mask = bit.band(second, 0x80) > 0
+  local mask = band(second, 0x80) > 0
   if mask then
     offset = offset + 4
   end
   if #chunk < offset + len - 1 then return end
 
-  local first = string.byte(chunk, 1)
-  local payload = string.sub(chunk, offset + 1, offset + len)
+  local first = byte(chunk, 1)
+  local payload = sub(chunk, offset + 1, offset + len)
   if mask then
-    payload = applyMask(payload, string.sub(chunk, offset - 3, offset))
+    payload = applyMask(payload, sub(chunk, offset - 3, offset))
   end
-  local extra = string.sub(chunk, offset + len + 1)
+  local extra = sub(chunk, offset + len + 1)
   return {
-    fin = bit.band(first, 0x80) > 0,
-    rsv1 = bit.band(first, 0x40) > 0,
-    rsv2 = bit.band(first, 0x20) > 0,
-    rsv3 = bit.band(first, 0x10) > 0,
-    opcode = bit.band(first, 0xf),
+    fin = band(first, 0x80) > 0,
+    rsv1 = band(first, 0x40) > 0,
+    rsv2 = band(first, 0x20) > 0,
+    rsv3 = band(first, 0x10) > 0,
+    opcode = band(first, 0xf),
     mask = mask,
     len = len,
     payload = payload
@@ -82,39 +95,44 @@ function exports.encode(item)
   local payload = item.payload
   assert(type(payload) == "string", "payload must be string")
   local len = #payload
-  if item.fin == nil then item.fin = true end
-  if item.opcode == nil then item.opcode = 2 end
+  local fin = item.fin
+  if fin == nil then fin = true end
+  local rsv1 = item.rsv1
+  local rsv2 = item.rsv2
+  local rsv3 = item.rsv3
+  local opcode = item.opcode or 2
+  local mask = item.mask
   local chars = {
-    string.char(bit.bor(
-      item.fin and 0x80 or 0,
-      item.rsv1 and 0x40 or 0,
-      item.rsv2 and 0x20 or 0,
-      item.rsv3 and 0x10 or 0,
-      item.opcode
+    char(bor(
+      fin and 0x80 or 0,
+      rsv1 and 0x40 or 0,
+      rsv2 and 0x20 or 0,
+      rsv3 and 0x10 or 0,
+      opcode
     )),
-    string.char(bit.bor(
-      item.mask and 0x80 or 0,
+    char(bor(
+      mask and 0x80 or 0,
       len < 0x10 and len or len < 0x10000 and 126 or 127
     ))
   }
   if len >= 0x10000 then
-    chars[3] = string.char(bit.band(bit.rshift(len, 56), 0xff))
-    chars[4] = string.char(bit.band(bit.rshift(len, 48), 0xff))
-    chars[5] = string.char(bit.band(bit.rshift(len, 40), 0xff))
-    chars[6] = string.char(bit.band(bit.rshift(len, 32), 0xff))
-    chars[7] = string.char(bit.band(bit.rshift(len, 24), 0xff))
-    chars[8] = string.char(bit.band(bit.rshift(len, 16), 0xff))
-    chars[9] = string.char(bit.band(bit.rshift(len, 8), 0xff))
-    chars[10] = string.char(bit.band(len, 0xff))
+    chars[3] = char(band(rshift(len, 56), 0xff))
+    chars[4] = char(band(rshift(len, 48), 0xff))
+    chars[5] = char(band(rshift(len, 40), 0xff))
+    chars[6] = char(band(rshift(len, 32), 0xff))
+    chars[7] = char(band(rshift(len, 24), 0xff))
+    chars[8] = char(band(rshift(len, 16), 0xff))
+    chars[9] = char(band(rshift(len, 8), 0xff))
+    chars[10] = char(band(len, 0xff))
   elseif len >= 0x10 then
-    chars[3] = string.char(bit.band(bit.rshift(len, 8), 0xff))
-    chars[4] = string.char(bit.band(len, 0xff))
+    chars[3] = char(band(rshift(len, 8), 0xff))
+    chars[4] = char(band(len, 0xff))
   end
-  if item.mask then
-    local mask = random(4)
-    return table.concat(chars) .. mask .. applyMask(payload, mask)
+  if mask then
+    local key = random(4)
+    return concat(chars) .. key .. applyMask(payload, key)
   end
-  return table.concat(chars) .. payload
+  return concat(chars) .. payload
 end
 
 local websocketGuid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
@@ -127,13 +145,13 @@ function exports.handshake(head, protocol)
   local headers = {}
   for i = 1, #head do
     local name, value = unpack(head[i])
-    headers[name:lower()] = value
+    headers[lower(name)] = value
   end
 
   -- Must have 'Upgrade: websocket' and 'Connection: Upgrade' headers
   if not (headers.upgrade and headers.connection
-      and headers.upgrade:lower() == "websocket"
-      and headers.connection:lower() == "upgrade"
+      and lower(headers.upgrade) == "websocket"
+      and lower(headers.connection) == "upgrade"
   ) then return end
 
   -- Make sure it's a new client speaking v13 of the protocol
@@ -151,7 +169,7 @@ function exports.handshake(head, protocol)
     local foundProtocol = false
     local list = headers["sec-websocket-protocol"]
     if list then
-      for item in string.gmatch(list, "[^, ]+") do
+      for item in gmatch(list, "[^, ]+") do
         if item == protocol then
           foundProtocol = true
           break
@@ -163,7 +181,7 @@ function exports.handshake(head, protocol)
     end
   end
 
-  local accept = base64(hexToBin(digest("sha1", key .. websocketGuid))):gsub("\n", "")
+  local accept = gsub(base64(hexToBin(digest("sha1", key .. websocketGuid))), "\n", "")
 
   local res = {
     code = 101,
