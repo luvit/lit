62ee717a92586030ae50c46fca650841ddaef0f2
Tim Caswell <tim@creationix.com>
2015-02-04 10:19:22 -0600

    Preserve parent env

--- app/commands/run.lua
+++ app/commands/run.lua
@@ -2,11 +2,21 @@ local uv = require('uv')
 
 local co = coroutine.running()
 
+local env = require('env')
+local newEnv = {}
+local keys = env.keys()
+for i = 1, #keys do
+  local key = keys[i]
+  local value = env.get(key)
+  if key ~= "LUVI_APP" then
+    newEnv[#newEnv + 1] = key .. "=" .. value
+  end
+end
+newEnv[#newEnv + 1] = "LUVI_APP=" .. uv.cwd()
+
 local child = uv.spawn(uv.exepath(), {
   args = args,
-  env = {
-    "LUVI_APP=" .. uv.cwd()
-  },
+  env = newEnv,
   stdio = {0,1,2}
 }, function (...)
   coroutine.resume(co, ...)
