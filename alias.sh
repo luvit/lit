3596b0d395185e6fe039d7e6076278280f7dab39
Tim Caswell <tim@creationix.com>
2015-02-17 15:01:20 -0600

    Cleanup dead and export as library

--- alias.sh
+++ alias.sh
@@ -1,3 +1,2 @@
 # source this to create a lit alias that runs directly our of this dir in dev mode.
-LUVI_BIN=`pwd`/luvi-binaries/`uname -s`_`uname -m`/luvi
-alias lit-dev=LUVI_APP=`pwd`' '$LUVI_BIN
+alias lit-dev=LUVI_APP=`pwd`' '`pwd`/luvi-binaries/`uname -s`_`uname -m`/luvi
