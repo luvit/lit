3d600ba0c208f441e852f53b4531af6a992e8f59
Tim Caswell <tim@creationix.com>
2015-02-18 17:29:46 +0000

    Require fixes in package.lua

--- package.lua
+++ package.lua
@@ -5,7 +5,7 @@ return {
     "creationix/pretty-print@0.1.0",
     "creationix/require@1.0.2",
     "creationix/coro-fs@1.2.3",
-    "creationix/coro-tcp@1.0.0",
+    "creationix/coro-tcp@1.0.1",
     "creationix/coro-tls@1.0.0",
     "creationix/hex-bin@1.0.0",
     "creationix/semver@1.0.1",
@@ -14,7 +14,7 @@ return {
     "creationix/ssh-rsa@0.1.2",
     "creationix/http-codec@0.1.4",
     "creationix/websocket-codec@1.0.0",
-    "creationix/json@2.5.0",
+    "creationix/json@2.5.1",
   },
   files = {
     "*.lua",
