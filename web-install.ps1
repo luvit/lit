dc1ef365ed8087b00614e3bc79da1f2f402235fc
Tim Caswell <tim@creationix.com>
2015-02-20 15:18:26 -0600

    Update powershell to use zip directly

--- web-install.ps1
+++ web-install.ps1
@@ -1,3 +1,7 @@
+
+$LIT_VERSION = "0.9.7"
+$LUVI_VERSION = "0.7.0"
+
 function Download-File {
 param (
   [string]$url,
@@ -9,16 +13,6 @@ param (
   $downloader.DownloadFile($url, $file)
 }
 
-$LIT_VERSION = "0.9.7"
-$LUVI_VERSION = "0.7.0"
-
-if ($env:TEMP -eq $null) {
-  $env:TEMP = Join-Path $env:SystemDrive 'temp'
-}
-$luviTempDir = Join-Path $env:TEMP "luvi"
-$tempDir = Join-Path $luviTempDir "lit"
-if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
-
 # download the package
 Write-Host "Download Luvi"
 $luviUrl = "https://github.com/luvit/luvi/releases/download/v$LUVI_VERSION/luvi-static-Windows-amd64.exe"
@@ -29,15 +23,7 @@ $litPackage = "https://github.com/luvit/lit/archive/$LIT_VERSION.zip"
 $litFile = "lit.zip"
 Download-File $litPackage $litFile
 
-# download 7zip
-Write-Host "Download 7Zip commandline tool"
-$7zaExe = Join-Path $tempDir '7za.exe'
-Download-File 'https://chocolatey.org/7za.exe' "$7zaExe"
-
 # Create Lit.exe
-Start-Process "$7zaExe" -ArgumentList "x -o`"$tempDir`" -y `"$litFile`"" -Wait -NoNewWindow
-$env:LUVI_APP="$tempDir\lit-$LIT_VERSION"
-$env:LUVI_TARGET="lit.exe"
-Start-Process "luvi.exe" -Wait -NoNewWindow
+$env:LUVI_APP="$litFile"
+Start-Process "luvi.exe" -ArgumentList "make $litFile" -Wait -NoNewWindow
 $env:LUVI_APP=""
-$env:LUVI_TARGET=""
