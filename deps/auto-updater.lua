d298e80f5567d3c9fa9a12788625ab187f33d8d6
Tim Caswell <tim@creationix.com>
2015-03-26 14:58:30 -0500

    Check for updates during autocore bootstrap

---
+++ deps/auto-updater.lua
@@ -0,0 +1,20 @@
+local request = require('coro-http').request
+local semver = require('semver')
+local jsonParse = require('json').parse
+
+-- Feed auto-updater your package.lua pre-parsed as a lua table
+function exports.check(meta)
+  local name = meta.name
+  local version = meta.version
+  assert(version and name, "Missing name or version in own metadata")
+  version = semver.normalize(version)
+  local head, body = request("GET", "http://lit.luvit.io/packages/" .. meta.name)
+  assert(head.code == 200)
+  local versions = assert(jsonParse(body), "Problem parsing JSON response from lit")
+  local key
+  return semver.match(version, function ()
+    local n = next(versions, key)
+    key = n
+    return key
+  end)
+end
