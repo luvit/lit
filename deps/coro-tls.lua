ed8c555dc623874cf71fd9f725fd06b7141d81e7
Tim Caswell <tim@creationix.com>
2015-04-23 13:00:48 -0500

    coro-tls: Fix server side

--- deps/coro-tls.lua
+++ deps/coro-tls.lua
@@ -1,5 +1,5 @@
 exports.name = "creationix/coro-tls"
-exports.version = "1.1.2"
+exports.version = "1.1.3"
 
 local openssl = require('openssl')
 local bit = require('bit')
@@ -38,7 +38,7 @@ exports.wrap = function (read, write, options)
     openssl.ssl.no_sslv3,
     openssl.ssl.no_compression))
   local bin, bout = openssl.bio.mem(8192), openssl.bio.mem(8192)
-  local ssl = ctx:ssl(bin, bout, false)
+  local ssl = ctx:ssl(bin, bout, options.server and true or false)
 
   local function flush()
     while bout:pending() > 0 do
@@ -50,8 +50,14 @@ exports.wrap = function (read, write, options)
   while true do
     if ssl:handshake() then break end
     flush()
-    bin:write(read())
+    local chunk = read()
+    if chunk then
+      bin:write(chunk)
+    else
+      error("disconnect while handshaking")
+    end
   end
+  flush()
 
   local done = false
   local function shutdown()
