d1b52ea73692e0aaa4c912479e13344aeff7ac4e
Tim Caswell <tim@creationix.com>
2015-04-23 13:15:21 -0500

    No need to use and/or to convert to boolean

--- deps/coro-tls.lua
+++ deps/coro-tls.lua
@@ -38,7 +38,7 @@ exports.wrap = function (read, write, options)
     openssl.ssl.no_sslv3,
     openssl.ssl.no_compression))
   local bin, bout = openssl.bio.mem(8192), openssl.bio.mem(8192)
-  local ssl = ctx:ssl(bin, bout, options.server and true or false)
+  local ssl = ctx:ssl(bin, bout, options.server)
 
   local function flush()
     while bout:pending() > 0 do
