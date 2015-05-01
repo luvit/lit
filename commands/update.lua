025aa1ea4da8359de20dd1aef742ea586e71b028
Tim Caswell <tim@creationix.com>
2015-05-01 13:43:11 -0500

    Update luvi and luvit as well with lit update

--- commands/update.lua
+++ commands/update.lua
@@ -1,33 +1,96 @@
-local meta = require('../package')
-local updater = require('auto-updater')
-local toupdate = updater.check(meta)
 local log = require('log')
-if not toupdate then
-  log("newer than remote", meta.version, "err")
-  return
-end
-if toupdate == meta.version then
-  log("up to date", meta.version, "highlight")
-  return
-end
-
-local core = require('autocore')
-local prompt = require('prompt')(require('pretty-print'))
+local updater = require('auto-updater')
 local uv = require('uv')
-local target = uv.exepath()
+local pathJoin = require('luvi').path.join
+local exec = require('exec')
+local prompt = require('prompt')(require('pretty-print'))
+local miniz = require('miniz')
+local binDir = pathJoin(uv.exepath(), "..")
 
-local res = prompt("Do you sure you wish to update " .. target .. " to lit version " .. toupdate .. "?", "Y/n")
-if not res:match("[yY]") then
-  log("canceled update", meta.version, "err")
-  return
+
+local function updateLit()
+  return updater.check(require('../package'), uv.exepath())
 end
 
+local function updateLuvit()
+  local luvitPath = pathJoin(binDir, "luvit")
+  if uv.fs_stat(luvitPath) then
+    local bundle = require('luvi').makeBundle({"/usr/local/bin/luvit"})
+    local fs = {
+      readFile = bundle.readfile,
+      stat = bundle.stat,
+    }
+    return updater.check(require('pkg').query(fs, "."), luvitPath)
+  else
+    return updater.check({ name = "luvit/luvit" }, luvitPath)
+  end
+end
 
-local new = target .. ".new"
-local old = target .. ".old"
-core.makeUrl("lit://" .. meta.name .. "@" .. toupdate, new)
-log("replacing binary", target, "highlight")
-uv.fs_rename(target, old)
-uv.fs_rename(new, target)
-uv.fs_unlink(old)
-log("update complete", toupdate, "success")
+local function updateLuvi()
+  local target = pathJoin(binDir, "luvi")
+  local new, old
+  local toupdate = require('luvi').version
+  if uv.fs_stat(target) then
+    local stdout = exec(target, "-v")
+    local version = stdout:match("luvi (v[^ \n]+)")
+    if version and version == toupdate then
+      log("luvi is up to date", version, "highlight")
+      return
+    end
+    log("found system luvi", version)
+    local res = prompt("Are you sure you wish to update " .. target .. " to luvi " .. toupdate .. "?", "Y/n")
+    if not res:match("[yY]") then
+      log("canceled update", version, "err")
+      return
+    end
+
+    log("updating luvi", toupdate)
+    new = target .. ".new"
+    old = target .. ".old"
+  else
+    log("installing luvi binary", target, "highlight")
+    old = nil
+    new = target
+  end
+
+  local fd = assert(uv.fs_open(new, "w", 511)) -- 0777
+  local source = uv.exepath()
+  local reader = miniz.new_reader(source)
+  local binSize
+  if reader then
+    -- If contains a zip, find where the zip starts
+    binSize = reader:get_offset()
+  else
+    -- Otherwise just read the file size
+    binSize = uv.fs_stat(source).size
+  end
+  local fd2 = assert(uv.fs_open(source, "r", 384)) -- 0600
+  assert(uv.fs_sendfile(fd, fd2, 0, binSize))
+  uv.fs_close(fd2)
+  uv.fs_close(fd)
+  if old then
+    log("replacing luvi binary", target, "highlight")
+    uv.fs_rename(target, old)
+    uv.fs_rename(new, target)
+    uv.fs_unlink(old)
+    log("luvi update complete", toupdate, "success")
+  else
+    log("luvi install complete", toupdate, "success")
+  end
+end
+
+local commands = {
+  luvi = updateLuvi,
+  luvit = updateLuvit,
+  lit = updateLit,
+  all = function ()
+    updateLit()
+    updateLuvit()
+    updateLuvi()
+  end
+}
+local cmd = commands[args[2] or "all"]
+if not cmd then
+  error("Unknown update command: " .. args[2])
+end
+cmd()
--- libs/auto-updater.lua
+++ libs/auto-updater.lua
@@ -1,14 +1,13 @@
 local request = require('coro-http').request
 local semver = require('semver')
 local jsonParse = require('json').parse
+local log = require('log')
+local prompt = require('prompt')(require('pretty-print'))
+local core = require('autocore')
+local uv = require('uv')
 
--- Feed auto-updater your package.lua pre-parsed as a lua table
-function exports.check(meta)
-  local name = meta.name
-  local version = meta.version
-  assert(version and name, "Missing name or version in own metadata")
-  version = semver.normalize(version)
-  local head, body = request("GET", "http://lit.luvit.io/packages/" .. meta.name)
+function exports.matchVersions(name, version)
+  local head, body = request("GET", "http://lit.luvit.io/packages/" .. name)
   assert(head.code == 200)
   local versions = assert(jsonParse(body), "Problem parsing JSON response from lit")
   local key
@@ -19,3 +18,49 @@ function exports.check(meta)
   end)
 end
 
+-- Feed auto-updater your package.lua pre-parsed as a lua table
+function exports.check(meta, target)
+  local name = meta.name
+  local basename = name:match("[^/]+$")
+  local version = meta.version
+  local toupdate, action
+  local new, old
+  if version then
+    version = semver.normalize(version)
+    toupdate = exports.matchVersions(name, version)
+    if not target then
+      return toupdate
+    end
+    if not toupdate then
+      log(basename .. " is newer than remote", meta.version, "err")
+      return
+    end
+    if toupdate == meta.version then
+      log(basename .. " is up to date", meta.version, "highlight")
+      return
+    end
+    local res = prompt("Are you sure you wish to update " .. target .. " to " .. name .. " version " .. toupdate .. "?", "Y/n")
+    if not res:match("[yY]") then
+      log("canceled " .. basename .. " update", meta.version, "err")
+      return
+    end
+    action = "update"
+    new = target .. ".new"
+    old = target .. ".old"
+  else
+    toupdate = exports.matchVersions(name)
+    action = "install"
+    new = target
+    old = nil
+  end
+
+  core.makeUrl("lit://" .. meta.name .. "@" .. toupdate, new)
+  log(action .. "ing " .. basename .. " binary", target, "highlight")
+  if old then
+    uv.fs_rename(target, old)
+    uv.fs_rename(new, target)
+    uv.fs_unlink(old)
+  end
+  log(basename .. " " .. action .. " complete", toupdate, "success")
+
+end
