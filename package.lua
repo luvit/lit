881fe26d2377c9adf45b31bbc7f12129c9e1f1e9
Tim Caswell <tim@creationix.com>
2015-06-04 16:48:13 -0500

    Bump lit version and luvi version

--- package.lua
+++ package.lua
@@ -1,13 +1,13 @@
 return {
   name = "luvit/lit",
-  version = "1.2.15",
+  version = "1.3.0",
   homepage = "https://github.com/luvit/lit",
   description = "The Luvit Invention Toolkit is a luvi app that handles dependencies and luvi builds.",
   tags = {"lit", "meta"},
   license = "MIT",
   author = { name = "Tim Caswell" },
   luvi = {
-    version = "2.0.9",
+    version = "2.1.0",
     flavor = "regular",
   },
   dependencies = {
