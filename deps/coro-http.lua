2cba73872e4e2109f70445ed57ec2c60dac6e18f
Tim Caswell <tim@creationix.com>
2015-06-24 17:42:20 -0500

    Publish updated coro-htto and websocket-codec packages

--- deps/coro-http.lua
+++ deps/coro-http.lua
@@ -1,5 +1,5 @@
 exports.name = "creationix/coro-http"
-exports.version = "1.1.0-1"
+exports.version = "1.1.1"
 exports.dependencies = {
   "creationix/coro-net@1.1.1",
   "creationix/coro-tls@1.2.0",
