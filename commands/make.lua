de0ccaf5d15bb371f67111f6152631e8d5e1d0f3
Martin Croome <martin.croome@gmail.com>
2015-07-10 10:06:37 +0200

    add option to lit make to allow use of different luvi for cross platform builds

--- commands/make.lua
+++ commands/make.lua
@@ -5,8 +5,9 @@ local pathJoin = require('luvi').path.join
 local cwd = uv.cwd()
 local source = args[2] and pathJoin(cwd, args[2])
 local target = args[3] and pathJoin(cwd, args[3])
+local luvi_source = args[4] and pathJoin(cwd, args[4])
 if not source or uv.fs_access(source, "r") then
-  core.make(source or cwd, target)
+  core.make(source or cwd, target, luvi_source)
 else
-  core.makeUrl(args[2], target)
+  core.makeUrl(args[2], target, luvi_source)
 end
