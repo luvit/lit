46a3c91fe035fa4834243daf7d7cece77f8fb292
Tim Caswell <tim@creationix.com>
2015-08-20 15:58:17 -0500

    Release new lit with updated luvi

--- get-lit.ps1
+++ get-lit.ps1
@@ -1,6 +1,6 @@
 
-$LUVI_VERSION = "2.2.0"
-$LIT_VERSION = "2.2.5"
+$LUVI_VERSION = "2.3.1"
+$LIT_VERSION = "2.2.6"
 
 if (test-path env:LUVI_ARCH) {
   $LUVI_ARCH = $env:LUVI_ARCH
--- get-lit.sh
+++ get-lit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-LUVI_VERSION=2.2.0
-LIT_VERSION=2.2.5
+LUVI_VERSION=2.3.1
+LIT_VERSION=2.2.6
 
 LUVI_ARCH=`uname -s`_`uname -m`
 LUVI_URL="https://github.com/luvit/luvi/releases/download/v$LUVI_VERSION/luvi-regular-$LUVI_ARCH"
