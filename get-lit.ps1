2d518b723dcf5073f3548684954cf7f79cf9502b
Tim Caswell <tim@creationix.com>
2015-08-26 13:19:45 -0500

    Publish version 2.2.9

--- get-lit.ps1
+++ get-lit.ps1
@@ -1,6 +1,6 @@
 
-$LUVI_VERSION = "2.3.1"
-$LIT_VERSION = "2.2.6"
+$LUVI_VERSION = "2.3.2"
+$LIT_VERSION = "2.2.9"
 
 if (test-path env:LUVI_ARCH) {
   $LUVI_ARCH = $env:LUVI_ARCH
--- package.lua
+++ package.lua
@@ -1,13 +1,13 @@
 return {
   name = "luvit/lit",
-  version = "2.2.8",
+  version = "2.2.9",
   homepage = "https://github.com/luvit/lit",
   description = "The Luvit Invention Toolkit is a luvi app that handles dependencies and luvi builds.",
   tags = {"lit", "meta"},
   license = "Apache 2",
   author = { name = "Tim Caswell" },
   luvi = {
-    version = "2.3.1",
+    version = "2.3.2",
     flavor = "regular",
   },
   dependencies = {
