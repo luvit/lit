5c85fbbbc0db10178740e476d0d0d29b30b9e628
Tim Caswell <tim@creationix.com>
2015-11-03 14:30:41 -0600

    Bump version to 2.2.14

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "luvit/lit",
-  version = "2.2.13",
+  version = "2.2.14",
   homepage = "https://github.com/luvit/lit",
   description = "The Luvit Invention Toolkit is a luvi app that handles dependencies and luvi builds.",
   tags = {"lit", "meta"},
