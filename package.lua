768d9c0cb72194c49f59ca782ff4c48826d35e2a
Tim Caswell <tim@creationix.com>
2015-12-16 16:42:17 -0600

    Update luvi to get zip timestamp fix

--- package.lua
+++ package.lua
@@ -1,13 +1,13 @@
 return {
   name = "luvit/lit",
-  version = "2.3.0",
+  version = "2.3.1",
   homepage = "https://github.com/luvit/lit",
   description = "The Luvit Invention Toolkit is a luvi app that handles dependencies and luvi builds.",
   tags = {"lit", "meta"},
   license = "Apache 2",
   author = { name = "Tim Caswell" },
   luvi = {
-    version = "2.5.0",
+    version = "2.5.1",
     flavor = "regular",
   },
   dependencies = {
