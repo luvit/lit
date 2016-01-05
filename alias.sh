1529420c185c545db4853790985811b3b0abf5ae
Tim Caswell <tim@creationix.com>
2016-01-05 15:02:38 -0600

    Remove old developer alias tools

--- alias.sh
+++
@@ -1,2 +0,0 @@
-# source this to create a lit-dev alias that runs directly our of this dir in dev mode.
-alias lit-dev=`which luvi`' '`pwd`' --'
--- dev.fish
+++
@@ -1,7 +0,0 @@
-# source this to create a lit-def function that runs directly our of this dir in dev mode.
-set lit_dir (pwd)
-set luvi_bin (pwd)/luvi
-
-function lit-dev
-  eval $luvi_bin $lit_dir -- $argv
-end
