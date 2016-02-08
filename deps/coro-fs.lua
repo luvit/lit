9d30024522c7b905f8f8b0c790e9461f32bc5d62
Tim Caswell <tim@creationix.com>
2016-02-08 10:05:12 -0600

    Publish new version of coro-fs that protects from luv change

--- deps/coro-fs.lua
+++ deps/coro-fs.lua
@@ -1,6 +1,6 @@
 --[[lit-meta
   name = "creationix/coro-fs"
-  version = "2.0.0"
+  version = "2.0.1"
   homepage = "https://github.com/luvit/lit/blob/master/deps/coro-fs.lua"
   description = "A coro style interface to the filesystem."
   tags = {"coro", "fs"}
