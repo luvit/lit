0be485bb8e13595b4cfdd6c22ed5a9ff29352896
Tim Caswell <tim@creationix.com>
2016-04-27 17:01:10 -0500

    Publish updated secure-socket with updated certs

--- deps/secure-socket/package.lua
+++ deps/secure-socket/package.lua
@@ -1,6 +1,6 @@
 return {
   name = "luvit/secure-socket",
-  version = "1.1.2",
+  version = "1.1.3",
   homepage = "https://github.com/luvit/luvit/blob/master/deps/secure-socket",
   description = "Wrapper for luv streams to apply ssl/tls",
   dependencies = {
