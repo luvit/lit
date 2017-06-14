6c743889f1663c9abe1f648e5bf3f17bc882d12a
Tim Caswell <tim@creationix.com>
2017-06-14 12:08:51 -0500

    Publish new websocket-codec 3.0.1

--- deps/websocket-codec.lua
+++ deps/websocket-codec.lua
@@ -1,7 +1,7 @@
 --[[lit-meta
   name = "creationix/websocket-codec"
   description = "A codec implementing websocket framing and helpers for handshakeing"
-  version = "3.0.0"
+  version = "3.0.1"
   dependencies = {
     "creationix/base64@2.0.0",
     "creationix/sha1@1.0.0",
