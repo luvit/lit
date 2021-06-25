9b0f3be64e9b8dd65bece121ce9a2ae8d8ac1c00
Tim Caswell <creationix@vercel.com>
2021-06-25 15:35:55 -0500

    Update luvi to v2.12.0 and release lit as v3.8.5

--- package.lua
+++ package.lua
@@ -1,13 +1,13 @@
 return {
   name = "luvit/lit",
-  version = "3.8.4",
+  version = "3.8.5",
   homepage = "https://github.com/luvit/lit",
   description = "The Luvit Invention Toolkit is a luvi app that handles dependencies and luvi builds.",
   tags = {"lit", "meta"},
   license = "Apache 2",
   author = { name = "Tim Caswell" },
   luvi = {
-    version = "v2.10.1",
+    version = "v2.12.0",
     flavor = "regular",
   },
   dependencies = {
