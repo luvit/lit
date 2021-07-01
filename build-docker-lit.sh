b86215776320580b94cd440ef48674f46a0cd626
Tim Caswell <creationix@vercel.com>
2021-07-01 09:41:00 -0500

    Update published docker container

--- build-docker-lit.sh
+++ build-docker-lit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo "Building latest luvi and lit for latest alpine using docker container..."
-docker build -t lit:alpine-dev - < Dockerfile.alpine-dev
+docker build -t lit:alpine-dev -f Dockerfile.alpine-dev .
 echo "Extracting lit and luvi binaries..."
 CONTAINER=$(docker create lit:alpine-dev)
 docker cp $CONTAINER:/luvi/build/luvi luvi
