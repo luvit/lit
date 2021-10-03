d89296d3af42532f8e8dbf2bb736f75e4c994974
RDW <duckwhale@mail.ru>
2021-10-03 03:46:35 +0200

    Fix autoformat
    Had to fix this manually, since the autoformatter has no option to change the behaviour (no idea why it doesn't line-break after the multiline string)...

--- main.lua
+++ main.lua
@@ -14,7 +14,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
---]] local bundle = require("luvi").bundle
+--]]
+
+local bundle = require("luvi").bundle
 loadstring(bundle.readfile("luvit-loader.lua"), "bundle:luvit-loader.lua")()
 
 -- Upvalues
