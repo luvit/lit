b9fd1584278333f15679d505dba9ac412ede0867
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-11-09 07:39:51 +0300

    fix & improve
    i got bad tips from spellcheck

--- update-lit.sh
+++ update-lit.sh
@@ -5,8 +5,6 @@ echo 'Checking for available updates...';
 GET_LIT=$(curl 'https://raw.githubusercontent.com/luvit/lit/master/get-lit.sh')
 
 shouldUpdate() {
-	use "$GET_LIT";
-
 	if ! command -v lit > /dev/null
 	then
 		echo 'Lit not installed';
@@ -26,26 +24,29 @@ shouldUpdate() {
 	fi
 
 	CUR_LIT=$(lit -v | grep -m1 -Po 'lit version:\s\K.*');
-	ACTUAL_LIT=$(echo "$GET_LIT" | grep -o -P "(?<=\${LIT_VERSION:-).*(?=})");
+	ACTUAL_LIT=$(echo "$GET_LIT" | grep -o -P '(?<=\${LIT_VERSION:-).*(?=})');
 
 	if [ "$CUR_LIT" != "$ACTUAL_LIT" ]; then
 		echo 'Lit needs update';
+		echo "$ACTUAL_LIT > $CUR_LIT";
 		return 0;
 	fi
 
-	CUR_LUVI=$(luvi -v | grep -m1 -Po 'luvi version:\s\K.*');
-	ACTUAL_LUVI=$(echo "$GET_LIT" | grep -o -P "(?<=\${LUVI_VERSION:-).*(?=})");
+	CUR_LUVI=$(luvi -v | grep -m1 -Po 'luvi\s\K.*');
+	ACTUAL_LUVI=$(echo "$GET_LIT" | grep -o -P '(?<=\${LUVI_VERSION:-).*(?=})');
 
 	if [ "$CUR_LUVI" != "$ACTUAL_LUVI" ]; then
 		echo 'Luvi needs update';
+		echo "$ACTUAL_LUVI > $CUR_LUVI";
 		return 0;
 	fi
 
 	CUR_LUVIT=$(luvit -v | grep -m1 -Po 'luvit version:\s\K.*');
-	ACTUAL_LUVIT=$(curl https://api.github.com/repos/luvit/luvit/releases/latest | grep -o -P "(?<=\"name\": \").*(?=\",)");
+	ACTUAL_LUVIT=$(curl https://api.github.com/repos/luvit/luvit/releases/latest | grep -o -P '(?<=\"name\": \").*(?=\",)');
 
 	if [ "$CUR_LUVIT" != "$ACTUAL_LUVIT" ]; then
 		echo 'Luvit needs update';
+		echo "$ACTUAL_LUVIT > $CUR_LUVIT";
 		return 0;
 	fi
 
