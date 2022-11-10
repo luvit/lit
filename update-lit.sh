620f5bfeaae0f38855559e40e647016f8549fa57
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-11-10 20:54:11 +0300

    rework
    1. arguments support
    2. now it does not depend on get-lit.sh
    3. console output has been improved, now the interaction with the script will more comfortable

--- update-lit.sh
+++ update-lit.sh
@@ -1,83 +1,123 @@
-#!/bin/sh
+#!/bin/bash
 
-echo 'Checking for available updates...';
+cecho() {
+	# foreground \033[48;2;RRR;GGG;BBBm
+	# background \033[38;2;RRR;GGG;BBBm
+	# clear      \033[0m
 
-GET_LIT=$(curl 'https://raw.githubusercontent.com/luvit/lit/master/get-lit.sh')
-
-shouldUpdate() {
-	if ! command -v lit > /dev/null
-	then
-		echo 'Lit is not installed';
-		return 0;
-	fi
-
-	if ! command -v luvi > /dev/null
-	then
-		echo 'Luvi is not installed';
-		return 0;
-	fi
-
-	if ! command -v luvit > /dev/null
-	then
-		echo 'Luvit is not installed';
-		return 0;
-	fi
-
-	echo 'Checking the lit version...';
-
-	CUR_LIT=$(lit -v | grep -m1 -Po 'lit version:\s\K.*');
-	ACTUAL_LIT=$(echo "$GET_LIT" | grep -o -P '(?<=\${LIT_VERSION:-).*(?=})');
-
-	echo "  Actual version: $ACTUAL_LIT";
-	echo "  Installed version: $CUR_LIT";
-
-	if [ "$CUR_LIT" = "$ACTUAL_LIT" ]; then
-		echo '  Latest version is installed';
-	else
-		echo '  Lit needs update';
-		echo "  $ACTUAL_LIT > $CUR_LIT";
-		return 0;
-	fi
-
-	echo 'Checking the luvi version...';
-
-	CUR_LUVI=$(luvi -v | grep -m1 -Po 'luvi\s\K.*');
-	ACTUAL_LUVI=$(echo "$GET_LIT" | grep -o -P '(?<=\${LUVI_VERSION:-).*(?=})');
-
-	echo "  Actual version: $ACTUAL_LUVI";
-	echo "  Installed version: $CUR_LUVI";
-
-	if [ "$CUR_LUVI" = "$ACTUAL_LUVI" ]; then
-		echo '  Latest version is installed';
-	else
-		echo '  Luvi needs update';
-		echo "  $ACTUAL_LUVI > $CUR_LUVI";
-		return 0;
-	fi
-
-	echo 'Checking the luvit version...';
-
-	CUR_LUVIT=$(luvit -v | grep -m1 -Po 'luvit version:\s\K.*');
-	ACTUAL_LUVIT=$(curl https://api.github.com/repos/luvit/luvit/releases/latest | grep -o -P '(?<=\"name\": \").*(?=\",)');
-
-	echo "  Actual version: $ACTUAL_LUVIT";
-	echo "  Installed version: $CUR_LUVIT";
-
-	if [ "$CUR_LUVIT" = "$ACTUAL_LUVIT" ]; then
-		echo '  Latest version is installed';
-	else
-		echo '  Luvit needs update';
-		echo "  $ACTUAL_LUVIT > $CUR_LUVIT";
-		return 0;
-	fi
-
-	return 1;
+	echo -e "\033[38;2;50;210;255m$1\033[0m";
 }
 
-cd '/usr/bin/' || exit;
+while test $# -gt 0; do
+	case "$1" in
+		-l|-last|--lastest )
+			cecho 'Checking the lit version...'
+			LIT_VERSION=$(curl https://api.github.com/repos/luvit/lit/tags | grep -o -P '(?<=\"name\": \").*(?=\",)' | head -1);
+			cecho "Lastest lit release: $LIT_VERSION";
 
-if shouldUpdate; then
-	echo 'Update...';
-	rm lit & rm luvi & rm luvit;
-	bash -c "$GET_LIT";
+			cecho 'Checking the luvi version...'
+			LUVI_VERSION=$(curl https://api.github.com/repos/luvit/luvi/releases/latest | grep -o -P '(?<=\"tag_name\": \"v).*(?=\",)');
+			cecho "Lastest luvi release: $LUVI_VERSION";
+
+			break;
+		;;
+		-lit-ver|--lit-version )
+			LIT_VERSION=$2;
+			shift 2;
+		;;
+		-luvi-ver|--luvi-version )
+			LUVI_VERSION=$2;
+			shift 2;
+		;;
+		-h|--help)
+		cecho 'luvit-update - a update tool for lit/luvi/luvit';
+		echo ' ';
+		cecho 'luvit-update [options] [arguments]';
+		echo ' ';
+		cecho 'options:';
+		cecho '-h, --help                             Show help.';
+		cecho '-l, -last, --lastest                   Download lastest lit/luvi/luvit releases from github (by default get-lit.sh downloads a bit outdated version, due to manual update).';
+		cecho '-lit-ver XXX, --lit-version YYY        Specify an lit version.';
+		cecho '-luvi-ver XXX, --luvi-version YYY      Specify an luvit version.';
+		exit 0;
+		;;
+		*)
+			break;
+		;;
+	esac
+done;
+
+if [ ! $LIT_VERSION ] || [ ! $LUVI_VERSION ]; then
+	cecho 'Request versions from get-lit.sh';
+	GET_LIT=$(curl 'https://raw.githubusercontent.com/luvit/lit/master/get-lit.sh');
+
+	if [ !$LIT_VERSION ]; then
+		LIT_VERSION=$(cecho "$GET_LIT" | grep -o -P '(?<=\${LIT_VERSION:-).*(?=})');
+		cecho "Lit release: $LIT_VERSION (may a bit outdated)";
+	fi
+	if [ !$LUVI_VERSION ]; then
+		LUVI_VERSION=$(cecho "$GET_LIT" | grep -o -P '(?<=\${LUVI_VERSION:-).*(?=})');
+		cecho "Luvi release: $LUVI_VERSION (may a bit outdated)";
+	fi
 fi
+
+update() {
+	LUVI_ARCH=`uname -s`_`uname -m`;
+	LUVI_URL="https://github.com/luvit/luvi/releases/download/v$LUVI_VERSION/luvi-regular-$LUVI_ARCH";
+	LIT_URL="https://lit.luvit.io/packages/luvit/lit/v$LIT_VERSION.zip";
+
+	rm lit 2> /dev/null & rm luvi 2> /dev/null & rm luvit 2> /dev/null;
+
+	# Download Files
+	cecho "Downloading $LUVI_URL to luvi"
+	curl -L -f -o luvi $LUVI_URL
+	chmod +x luvi
+	cecho "Downloading $LIT_URL to lit.zip"
+	curl -L -f -o lit.zip $LIT_URL
+	# Create lit using lit
+	./luvi lit.zip -- make lit.zip lit luvi
+	# Cleanup
+	rm -f lit.zip
+	# Create luvit using lit
+	./lit make lit://luvit/luvit luvit luvi
+
+	exit 0;
+}
+
+if ! command -v lit > /dev/null
+then
+	cecho '  Lit is not installed';
+	update;
+fi
+
+if ! command -v luvi > /dev/null
+then
+	cecho '  Luvi is not installed';
+	update;
+fi
+
+if ! command -v luvit > /dev/null
+then
+	cecho '  Luvit is not installed';
+	update;
+fi
+
+LIT_VERSION_CUR=$(lit -v | grep -m1 -Po 'lit version:\s\K.*');
+cecho "Installed version of lit: $LIT_VERSION_CUR";
+
+if [ "$LIT_VERSION" != "$LIT_VERSION_CUR" ]; then
+	cecho '  Lit needs update';
+	cecho "  $LIT_VERSION != $LIT_VERSION_CUR";
+	update;
+fi
+
+LUVI_VERSION_CUR=$(luvi -v | grep -m1 -Po 'luvi v\K.*');
+cecho "Installed version of luvi: $LUVI_VERSION_CUR";
+
+if [ "$LUVI_VERSION" != "$LUVI_VERSION_CUR" ]; then
+	cecho '  Luvi needs update';
+	cecho "  $LUVI_VERSION != $LUVI_VERSION_CUR";
+	update;
+fi
+
+cecho 'No update required';
